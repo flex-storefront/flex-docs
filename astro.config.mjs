abd03ef97d54339049b84f87a8aa36c8d43dd504
Stephen Richter <stephenrichter15@gmail.com>
2024-04-25 09:09:47 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://base1.com",
-  integrations: [preact()]
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
